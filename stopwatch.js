fd26999c2d76f9512f38e516ac02fcdfdf5a9a8d
James Paik <jpaik456@gmail.com>
2017-06-23 12:29:44 -0400

    Add tournament mode changes

--- stopwatch.js
+++ stopwatch.js
@@ -1,8 +1,12 @@
 var time = {'minutes': 0, 'seconds': 0, 'centiseconds': 0};
-var timer, running = false;
+var timer, running = false, mode = "normal";
 var timeDiv = document.getElementById('time'); //Span that hold time string
+var modeButton = document.getElementById('mode'); //Clicking mode
 
 document.addEventListener('keydown', handleTimer); //On pressing space
+document.addEventListener('keyup', handleTournamentTimer); //On letting go of space
+
+modeButton.addEventListener('click', changeMode); //Change mode on button click
 
 //Absolutely position at center
 setTimeout(function(){
@@ -11,16 +15,47 @@ setTimeout(function(){
 }, 300); //Giving it time to render
 
 function handleTimer(e){ //Sends it to correct function based on running
-  var keyCode = e.keyCode;  
+  var keyCode = e.keyCode;
   if(keyCode !== 32) return false;
-  console.log("Init stopwatch");
-  if(running){ //Stop time
+
+  if(running || mode === "tournament"){ //Stop time
     stopTime();
-  } else { //Run Time    
+  } else { //Run Time
     Object.keys(time).forEach(x => time[x] = 0); //Reset
     timeDiv.innerHTML = getTimeString();
     running = true;
     timer = setInterval(runTime, 10); //Runs every 10 millisecond
+  }
+}
+
+function handleTournamentTimer(e){
+  if(mode === "normal") return;
+  var keyCode = e.keyCode;
+  if(keyCode !== 32) return false;
+  if(!running){
+    Object.keys(time).forEach(x => time[x] = 0); //Reset
+    timeDiv.innerHTML = getTimeString();
+    running = true;
+    timer = setInterval(runTime, 10); //Runs every 10 millisecond
+  }
+}
+
+function changeMode(){
+  if(running){
+    stopTime();
+    Object.keys(time).forEach(x => time[x] = 0); //Reset
+    timeDiv.innerHTML = getTimeString();
+  }
+  if(modeButton.dataset.mode === "normal"){ //Switch to tournament
+    document.querySelector('.normal').style.display = "none";
+    document.querySelector('.tournament').style.display = "inline-block";
+    modeButton.dataset.mode = "tournament";
+     mode = "tournament";
+  } else {
+    document.querySelector('.normal').style.display = "inline-block";
+    document.querySelector('.tournament').style.display = "none";
+    modeButton.dataset.mode = "normal";
+    mode = "normal";
   }
 }
 
@@ -38,7 +73,7 @@ function runTime(){
   if(time['seconds'] >= 60){
     time['seconds'] = 0;
     time['minutes']++;
-  }  
+  }
   timeDiv.innerHTML = getTimeString();
 }
 
